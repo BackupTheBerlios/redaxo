7b87a084dab22c859a57f03ff6686f842e42631a
kristinus <kristinus>
2005-11-08 15:34:11 +0000

    wenn kein gif support und thumbresize an, dann kein thumbaufruf..

--- redaxo3_0/redaxo/include/pages/medienpool.inc.php
+++ redaxo3_0/redaxo/include/pages/medienpool.inc.php
@@ -2,7 +2,7 @@
 /** 
  *  
  * @package redaxo3 
- * @version $Id: medienpool.inc.php,v 1.74 2005/10/30 23:48:44 koala_s Exp $ 
+ * @version $Id: medienpool.inc.php,v 1.75 2005/11/08 15:34:11 kristinus Exp $ 
  */ 
 
 // TODOS
@@ -884,7 +884,9 @@ if (isset($subpage) and $subpage == "detail")
       if ($ffiletype_ii)
       {
         $imgn = "../files/$fname width=$rfwidth";
-        if ($thumbs && $thumbsresize && $rfwidth>199) $imgn = "../index.php?rex_resize=200w__$fname";
+        
+        if ($thumbs && $thumbsresize && $ffiletype == "image/gif" && $rfwidth>199 && !function_exists(imageGIF)) $imgn = "../files/$fname width=200";
+        else if ($thumbs && $thumbsresize && $rfwidth>199) $imgn = "../index.php?rex_resize=200w__$fname ";
         echo "<td rowspan=12 width=220 align=center class=lgrey valign=top><br><img src=$imgn  border=0></td>";
       }
 
