e7e2ed12b9d344adedbf9ab9b80130dbb5a63fff
kristinus <kristinus>
2005-12-27 15:36:11 +0000

    ep erstellt

--- redaxo3_0/redaxo/include/addons.inc.php
+++ redaxo3_0/redaxo/include/addons.inc.php
@@ -1,5 +1,11 @@
 <?php
 
+/** 
+ * Addonlist
+ * @package redaxo3 
+ * @version $Id: addons.inc.php,v 1.5 2005/12/27 15:36:11 kristinus Exp $ 
+ */ 
+
 // ----------------- addons
 if (isset($REX['ADDON']['status'])) {
   unset($REX['ADDON']['status']);
@@ -11,10 +17,7 @@ if (isset($REX['ADDON']['status'])) {
 $REX['ADDON']['install']['import_export'] = 1;
 $REX['ADDON']['status']['import_export'] = 1;
 
-$REX['ADDON']['install']['shop'] = 0;
-$REX['ADDON']['status']['shop'] = 0;
-
-// --- /DYN
+// --- /DYN
 // ----------------- /DONT EDIT BELOW THIS
 
 
@@ -23,5 +26,8 @@ for($i=0;$i<count($REX['ADDON']['status']);$i++)
 	if (current($REX['ADDON']['status']) == 1) include $REX['INCLUDE_PATH']."/addons/".key($REX['ADDON']['status'])."/config.inc.php";
 	next($REX['ADDON']['status']);
 }
+
+// ----- all addons configs included
+rex_register_extension_point( 'ADDONS_INCLUDED');
 
 ?>
