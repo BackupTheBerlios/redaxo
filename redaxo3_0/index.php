e1db5bb07400c5107b54883b7f5f0b636310d600
kills <kills>
2006-05-25 18:07:45 +0000

    - default article_id wird jetzt in der master.inc gesetzt, damit auch im Backend verfügbar

--- redaxo3_0/index.php
+++ redaxo3_0/index.php
@@ -3,7 +3,7 @@
 /** 
  * 
  * @package redaxo3 
- * @version $Id: index.php,v 1.30 2006/03/22 15:30:41 kristinus Exp $ 
+ * @version $Id: index.php,v 1.31 2006/05/25 18:07:45 kills Exp $ 
  */ 
 
 
@@ -37,26 +37,25 @@ $REX['GG'] = true;
 
 
 // setzte pfad und includiere klassen und funktionen
-
 $REX['HTDOCS_PATH'] = "./";
 include "./redaxo/include/master.inc.php";
-
 
 // Starte einen neuen Artikel und setzte die aktuelle
 // artikel id. wenn nicht vorhanden, nimm einen
 // speziellen artikel. z.b. fehler seite oder home seite
 
-if (!isset($article_id) or $article_id == '') $article_id = $REX['START_ARTICLE_ID'];
-
 $REX_ARTICLE = new article;
 $REX_ARTICLE->setCLang($clang);
+
 if ($REX_ARTICLE->setArticleId($article_id))
 {
   echo $REX_ARTICLE->getArticleTemplate();
-}elseif($REX_ARTICLE->setArticleId($REX['NOTFOUND_ARTICLE_ID']))
+}
+elseif($REX_ARTICLE->setArticleId($REX['NOTFOUND_ARTICLE_ID']))
 {
   echo $REX_ARTICLE->getArticleTemplate();
-}else
+}
+else
 {
   echo 'Kein Startartikel selektiert / No starting Article selected. Please click here to enter <a href="redaxo/index.php">redaxo</a>';
   $REX['STATS'] = 0;
