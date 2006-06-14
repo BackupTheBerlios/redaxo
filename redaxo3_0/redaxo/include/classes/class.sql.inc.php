2704e1022a5dc7188474ac2850b1cad665aa1be0
kills <kills>
2006-06-14 13:35:10 +0000

    - getServerVersion() implementiert

--- redaxo3_0/redaxo/include/classes/class.sql.inc.php
+++ redaxo3_0/redaxo/include/classes/class.sql.inc.php
@@ -12,7 +12,7 @@
 
 /**
  * Klasse zur Verbindung und Interatkion mit der Datenbank
- * @version $Id: class.sql.inc.php,v 1.29 2006/06/03 21:21:38 kills Exp $ 
+ * @version $Id: class.sql.inc.php,v 1.30 2006/06/14 13:35:10 kills Exp $ 
  */
 class sql
 {
@@ -52,21 +52,6 @@ class sql
       preg_match('/([0-9]+\.([0-9\.])+)/', $res[0]['VERSION'], $arr);
       $REX['MYSQL_VERSION'] = $arr[1];
     }
-  }
-  /**
-   * Statische Funktion zur Abfrage der Serverversion
-   */
-  function getServerVersion()
-  {
-    global $REX;
-    
-    if(empty($REX['MYSQL_VERSION']))
-    {
-      // Die Version wird im Konstrukor abgefragt
-      $tmp = new sql();
-    }
-    
-    return $REX['MYSQL_VERSION'];
   }
 
   /**
