5447614b5bcb6639ef9a445d74cf4ef366291e88
kills <kills>
2006-07-04 14:07:22 +0000

    - Bugfix, falls bei REX_PHP ein ParseError vorliegt

--- redaxo3_0/redaxo/include/classes/variables/class.rex_var_value.inc.php
+++ redaxo3_0/redaxo/include/classes/variables/class.rex_var_value.inc.php
@@ -4,7 +4,7 @@
 /**
  * REX_VALUE[1], REX_HTML_VALUE[1], REX_PHP, REX_PHP_VALUE[1], REX_HTML, REX_IS_VALUE
  * @package redaxo3
- * @version $Id: class.rex_var_value.inc.php,v 1.2 2006/07/04 13:52:41 kills Exp $
+ * @version $Id: class.rex_var_value.inc.php,v 1.3 2006/07/04 14:07:22 kills Exp $
  */
 
 class rex_var_value extends rex_var
@@ -24,16 +24,16 @@ class rex_var_value extends rex_var
     return $content;
   }
 
-  function getFEOutput(& $sql, $content)
-  {
-    $content = $this->getOutput($sql, $content, true);
-    $content = str_replace('REX_PHP', $sql->getValue('php'), $content);
-    return $content;
-  }
-
   function getBEInput(& $sql, $content)
   {
     $content = $this->getOutput($sql, $content);
+    $content = str_replace('REX_PHP', $this->stripPHP($sql->getValue('php')), $content);
+    return $content;
+  }
+  
+  function getFEOutput(& $sql, $content)
+  {
+    $content = $this->getOutput($sql, $content, true);
     $content = str_replace('REX_PHP', $sql->getValue('php'), $content);
     return $content;
   }
