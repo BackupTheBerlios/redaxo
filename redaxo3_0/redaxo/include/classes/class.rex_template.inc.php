d40db2d65712f7a66582bae8c04c78958c46fa91
kills <kills>
2007-03-25 17:55:59 +0000

    im constructor jetzt auch mit setId() neu formatiert

--- redaxo3_0/redaxo/include/classes/class.rex_template.inc.php
+++ redaxo3_0/redaxo/include/classes/class.rex_template.inc.php
@@ -5,38 +5,38 @@
  * Zuständig für die Verarbeitung eines Templates
  * 
  * @package redaxo3
- * @version $Id: class.rex_template.inc.php,v 1.2 2007/03/25 17:02:18 kristinus Exp $
+ * @version $Id: class.rex_template.inc.php,v 1.3 2007/03/25 17:55:59 kills Exp $
  */
- 
+
 class rex_template
 {
-   var $id;
-   
-   function rex_template($template_id)
-   {
-      $this->id = $template_id;
-   }
-   
-   function getId()
-   {
-      return $this->id;
-   }
-   
-   function setId($id)
-   {
-      $this->id = (int) $id;
-   }
-   
-   function getTemplate()
-   {
-      global $REX;
-      
-      $file = $REX['INCLUDE_PATH']. '/generated/templates/'. $this->getId() .'.template';
-      $handle = fopen($file, 'r');
-      $content = fread($handle, filesize($file));
-      fclose($handle);
-      
-      return $content;
-   }
+  var $id;
+
+  function rex_template($template_id)
+  {
+    $this->setId($template_id);
+  }
+
+  function getId()
+  {
+    return $this->id;
+  }
+
+  function setId($id)
+  {
+    $this->id = (int) $id;
+  }
+
+  function getTemplate()
+  {
+    global $REX;
+
+    $file = $REX['INCLUDE_PATH'] . '/generated/templates/' . $this->getId() . '.template';
+    $handle = fopen($file, 'r');
+    $content = fread($handle, filesize($file));
+    fclose($handle);
+
+    return $content;
+  }
 }
 ?>
