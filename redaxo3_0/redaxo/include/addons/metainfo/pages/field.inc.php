aa0305bcfa16a86e3927b92dc7e2f17463e6e34e
kills <kills>
2007-05-05 09:40:37 +0000

    neu formatiert

--- redaxo3_0/redaxo/include/addons/metainfo/pages/field.inc.php
+++ redaxo3_0/redaxo/include/addons/metainfo/pages/field.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: field.inc.php,v 1.5 2007/04/28 18:11:29 kristinus Exp $
+ * @version $Id: field.inc.php,v 1.6 2007/05/05 09:40:37 kills Exp $
  */
  
 //------------------------------> Parameter
@@ -28,12 +28,9 @@ if ($func == '')
 	$list->addColumn('<a href="index.php?page='.$page.'&amp;subpage='.$subpage.'&amp;func=add"><img src="pics/modul_plus.gif" alt="add" title="add" /></a>', '<img src="pics/modul.gif" alt="field" title="field" />', 0, array('<th class="rex-icon">###VALUE###</th>','<td class="rex-icon">###VALUE###</td>'));
 	
 	$list->setColumnLabel('field_id', $I18N_META_INFOS->msg('field_label_id'));
-
-
-$list->setColumnLayout('field_id',  array('<th class="rex-icon">###VALUE###</th>','<td class="rex-icon">###VALUE###</td>'));
+	$list->setColumnLayout('field_id',  array('<th class="rex-icon">###VALUE###</th>','<td class="rex-icon">###VALUE###</td>'));
 
 	$list->setColumnLabel('name', $I18N_META_INFOS->msg('field_label_name'));
-	
 	$list->setColumnParams('name', array('func' => 'edit', 'field_id' => '###field_id###'));
 	
 	$list->show();
