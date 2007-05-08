add3c580484103a3f22da72e2ac2ccd23b4e34ad
tbaddade <tbaddade>
2007-05-08 09:42:36 +0000

    Hinweis auf Benutzrecht hinzugefgt

--- redaxo3_0/redaxo/include/addons/textile/pages/index.inc.php
+++ redaxo3_0/redaxo/include/addons/textile/pages/index.inc.php
@@ -6,12 +6,16 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: index.inc.php,v 1.4 2007/05/08 08:09:11 kills Exp $
+ * @version $Id: index.inc.php,v 1.5 2007/05/08 09:42:36 tbaddade Exp $
  */
  
 require $REX['INCLUDE_PATH'].'/layout/top.php';
 
 rex_title('Textile');
+
+
+$mdl_help = '<?php rex_a79_help_overview(); ?>';
+
 
 $mdl_ex =<<<EOD
 <?php
@@ -25,22 +29,26 @@ if (REX_IS_VALUE[1])
 ?>
 EOD;
 
-$mdl_help = '<?php rex_a79_help_overview(); ?>';
-
 ?>
 
 <div class="rex-addon-output">
-	<h2><?php echo $I18N_A79->msg('module_headline'); ?></h2>
+	<h2><?php echo $I18N_A79->msg('code_for_module_input'); ?></h2>
+	
+	<div class="rex-addon-content">	
+		<p><?php echo $I18N_A79->msg('module_intro_help'); ?></p>
+	
+		<code><?php echo htmlspecialchars($mdl_help); ?></code>
+		
+		<p><?php echo $I18N_A79->msg('module_rights'); ?></p>
+	</div>
+	
+	<h2><?php echo $I18N_A79->msg('code_for_module_output'); ?></h2>
 	
 	<div class="rex-addon-content">
 		<p><?php echo $I18N_A79->msg('module_intro_moduleoutput'); ?></p>
 	
 		<h3><?php echo $I18N_A79->msg('example_for'); ?> REX_VALUE[1]</h3>
 		<?php highlight_string($mdl_ex); ?>
-	
-		<p><?php echo $I18N_A79->msg('module_intro_help'); ?></p>
-	
-		<code><?php echo htmlspecialchars($mdl_help); ?></code>
 	</div>
 </div>
 
