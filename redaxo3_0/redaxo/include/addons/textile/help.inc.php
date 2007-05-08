fb480ecb0b60d0687b7f65d14fde7ae178777748
kills <kills>
2007-05-08 20:57:02 +0000

    *** empty log message ***

--- redaxo3_0/redaxo/include/addons/textile/help.inc.php
+++ redaxo3_0/redaxo/include/addons/textile/help.inc.php
@@ -1,20 +1,16 @@
 <?php
 
 /**
- * TinyMCE Addon
+ * Textile Addon
  *  
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
- * 
- * @author Dave Holloway
- * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>s
- * 
  * @package redaxo3
- * @version $Id: help.inc.php,v 1.4 2007/05/08 20:57:47 kills Exp $
+ * @version $Id: help.inc.php,v 1.3 2007/05/08 20:57:02 kills Exp $
  */
 
 ?>
-<b>TinyMCE Addon</b>
+<b>Textile Addon</b>
 
 <br /><br />
-Erweitert REDAXO um den WYSIWYG-Editor, TinyMCE
+Bringt die Möglichkeit in Modulen Textile Markup zu verwenden
