23d0b8829deb59054615f8e795d56cc6664659e3
kills <kills>
2007-05-11 16:03:02 +0000

    optionales feld für select,radio,checkbox eingebaut

--- redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
+++ redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
@@ -5,10 +5,11 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: extension_common.inc.php,v 1.1 2007/05/05 09:39:38 kills Exp $
+ * @version $Id: extension_common.inc.php,v 1.2 2007/05/11 16:03:02 kills Exp $
  */
  
 rex_register_extension('ALL_GENERATED', 'rex_a62_metainfo_regenerate_all');
+rex_register_extension('OUTPUT_FILTER', 'rex_a62_insertJs');
 
 /**
  * Führt das nötige Cleanup nach einem "regenerate all" her. 	
@@ -16,4 +17,29 @@ rex_register_extension('ALL_GENERATED', 'rex_a62_metainfo_regenerate_all');
 function rex_a62_metainfo_regenerate_all($params)
 {
 	rex_set_session('A62_MESSAGE', '');
+}
+
+/**
+ * Fügt den nötigen JS-Code ein
+ */
+function rex_a62_insertJs($params)
+{
+	global $REX;
+	
+	$content = $params['subject'];
+
+	$jsfile = $REX['INCLUDE_PATH'] .'/addons/metainfo/js/metainfo.js';
+	$hdl = fopen($jsfile, 'r');
+	$jscontent = fread($hdl, filesize($jsfile));
+	fclose($hdl);
+  
+  $js ='
+	  <script type="text/javascript">
+	  <!--
+	  '. $jscontent .'
+	  //-->
+	  </script>
+  ';
+
+  return str_replace('</head>', $js . '</head>', $content);
 }
