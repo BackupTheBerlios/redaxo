1041f4e9516f0e22ce6a9f29c246bac9c60113ab
kills <kills>
2007-06-28 09:53:01 +0000

    default sind keine addons installiert. system addons werden im setup automatisch installiert+aktiviert

--- redaxo3_0/redaxo/include/addons.inc.php
+++ redaxo3_0/redaxo/include/addons.inc.php
@@ -3,7 +3,7 @@
 /** 
  * Addonlist
  * @package redaxo3 
- * @version $Id: addons.inc.php,v 1.5 2005/12/27 15:36:11 kristinus Exp $ 
+ * @version $Id: addons.inc.php,v 1.6 2007/06/28 09:53:01 kills Exp $ 
  */ 
 
 // ----------------- addons
@@ -14,17 +14,17 @@ if (isset($REX['ADDON']['status'])) {
 // ----------------- DONT EDIT BELOW THIS
 // --- DYN
 
-$REX['ADDON']['install']['import_export'] = 1;
-$REX['ADDON']['status']['import_export'] = 1;
-
 // --- /DYN
 // ----------------- /DONT EDIT BELOW THIS
 
 
-for($i=0;$i<count($REX['ADDON']['status']);$i++)
+if(isset($REX['ADDON']) && is_array($REX['ADDON']))
 {
-	if (current($REX['ADDON']['status']) == 1) include $REX['INCLUDE_PATH']."/addons/".key($REX['ADDON']['status'])."/config.inc.php";
-	next($REX['ADDON']['status']);
+  for($i=0;$i<count($REX['ADDON']['status']);$i++)
+  {
+  	if (current($REX['ADDON']['status']) == 1) include $REX['INCLUDE_PATH']."/addons/".key($REX['ADDON']['status'])."/config.inc.php";
+  	next($REX['ADDON']['status']);
+  }
 }
 
 // ----- all addons configs included
