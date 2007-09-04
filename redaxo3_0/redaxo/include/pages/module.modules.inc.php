e03d8aef20104be6b103b62147b006b1afb31fab
tbaddade <tbaddade>
2007-09-04 18:41:56 +0000

    Breite und Hhe der Icons herausgenommenhttp://forum.redaxo.de/ftopic5789.html

--- redaxo3_0/redaxo/include/pages/module.modules.inc.php
+++ redaxo3_0/redaxo/include/pages/module.modules.inc.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package redaxo3
- * @version $Id: module.modules.inc.php,v 1.35 2007/09/02 13:46:41 kills Exp $
+ * @version $Id: module.modules.inc.php,v 1.36 2007/09/04 18:41:56 tbaddade Exp $
  */
 
 $OUT = TRUE;
@@ -315,7 +315,7 @@ if ($OUT)
     </colgroup>
     <thead>
       <tr>
-        <th class="rex-icon"><a href="index.php?page=module&amp;function=add"><img src="pics/modul_plus.gif" width="16" height="16" alt="'.$I18N->msg("create_module").'" title="'.$I18N->msg("create_module").'" /></a></th>
+        <th class="rex-icon"><a href="index.php?page=module&amp;function=add"><img src="pics/modul_plus.gif" alt="'.$I18N->msg("create_module").'" title="'.$I18N->msg("create_module").'" /></a></th>
         <th class="rex-icon">ID</th>
         <th>'.$I18N->msg('module_description').'</th>
         <th>'.$I18N->msg('module_functions').'</th>
@@ -332,7 +332,7 @@ if ($OUT)
 
     echo '
       <tr>
-        <td class="rex-icon"><a href="index.php?page=module&amp;modul_id='.$sql->getValue("id").'&amp;function=edit"><img src="pics/modul.gif" width="16" height="16" alt="'. $sql->getValue("name") .'" title="'. $sql->getValue("name") .'"/></a></td>
+        <td class="rex-icon"><a href="index.php?page=module&amp;modul_id='.$sql->getValue("id").'&amp;function=edit"><img src="pics/modul.gif" alt="'. $sql->getValue("name") .'" title="'. $sql->getValue("name") .'"/></a></td>
         <td class="rex-icon">'.$sql->getValue("id").'</td>
         <td><a href="index.php?page=module&amp;modul_id='.$sql->getValue("id").'&amp;function=edit">'.htmlspecialchars($sql->getValue("name")).'</a></td>
         <td><a href="index.php?page=module&amp;modul_id='.$sql->getValue("id").'&amp;function=delete" onclick="return confirm(\''.$I18N->msg('delete').' ?\')">'.$I18N->msg("delete_module").'</a></td>
