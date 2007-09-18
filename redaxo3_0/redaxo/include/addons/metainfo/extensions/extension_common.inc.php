3d39f8b519d87494c72abdc623cfe0a9a03036bb
kills <kills>
2007-09-18 14:55:05 +0000

    warnung gefixed, da im saveCallback schonmal über die sqlFields iteriert wurde

--- redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
+++ redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: extension_common.inc.php,v 1.17 2007/09/16 14:51:21 kills Exp $
+ * @version $Id: extension_common.inc.php,v 1.18 2007/09/18 14:55:05 kills Exp $
  */
 
 if($page == 'metainfo')
@@ -56,6 +56,7 @@ function rex_a62_metaFields($sqlFields, $activeItem, $formatCallback, $epParams)
   $mlist_id = 1;
   $link_id  = 1;
 
+  $sqlFields->reset();
   for($i = 0; $i < $sqlFields->getRows(); $i++)
   {
     // Umschliessendes Tag von Label und Formularelement
