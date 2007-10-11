35ce88d4b1a91d112128f4df3f7ab2fe039f540f
kills <kills>
2007-10-11 16:48:58 +0000

    radios/checkboxen beautified

--- redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
+++ redaxo3_0/redaxo/include/addons/metainfo/extensions/extension_common.inc.php
@@ -5,7 +5,7 @@
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  * @package redaxo3
- * @version $Id: extension_common.inc.php,v 1.21 2007/10/11 16:45:19 kills Exp $
+ * @version $Id: extension_common.inc.php,v 1.22 2007/10/11 16:48:58 kills Exp $
  */
 
 if($page == 'metainfo')
@@ -155,7 +155,7 @@ function rex_a62_metaFields($sqlFields, $activeItem, $formatCallback, $epParams)
           else
           {
             $field .= '<p class="'. $class .'">'."\n";
-            $field .= '<label for="'. $id .'">'. htmlspecialchars($value) .'</label>';
+            $field .= '<label for="'. $id .'"><span>'. htmlspecialchars($value) .'</span></label>';
             $field .= '<input type="'. $typeLabel .'" name="'. $name .'" value="'. $key .'" id="'. $id .'" '. $attr . $selected .' />'."\n";
             $field .= '</p>'."\n";
           }
