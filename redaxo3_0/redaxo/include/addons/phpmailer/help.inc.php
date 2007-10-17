5a8dcaf3248343041fcff1fbf7cabfe227ee2c44
kills <kills>
2007-10-17 09:21:31 +0000

    changelog eingebaut

--- redaxo3_0/redaxo/include/addons/phpmailer/help.inc.php
+++ redaxo3_0/redaxo/include/addons/phpmailer/help.inc.php
@@ -2,15 +2,23 @@
 
 /**
  * PHPMailer Addon
- *  
+ *
  * @author staab[at]public-4u[dot]de Markus Staab
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
- * 
+ *
  * @package redaxo4
- * @version $Id: help.inc.php,v 1.3 2007/10/13 13:52:01 kills Exp $
+ * @version $Id: help.inc.php,v 1.4 2007/10/17 09:21:31 kills Exp $
  */
 
 ?>
-<b>PHPMailer Addon</b>
+<p>
+PHPMailer Addon
 
 <br /><br />
+
+<?php
+  $file = dirname( __FILE__) .'/_changelog.txt';
+  if(is_readable($file))
+    echo str_replace( '+', '&nbsp;&nbsp;+', nl2br(file_get_contents($file)));
+?>
+</p>
