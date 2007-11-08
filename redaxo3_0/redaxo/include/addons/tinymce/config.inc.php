b5efcfb646b760b790354dd6564fe9fb01304744
kills <kills>
2007-11-08 21:00:47 +0000

    " ->'

--- redaxo3_0/redaxo/include/addons/tinymce/config.inc.php
+++ redaxo3_0/redaxo/include/addons/tinymce/config.inc.php
@@ -10,17 +10,17 @@
  * @author <a href="http://www.GN2-Netwerk.de">www.GN2-Netwerk.de</a>s
  *
  * @package redaxo4
- * @version $Id: config.inc.php,v 1.10 2007/10/18 18:27:02 kills Exp $
+ * @version $Id: config.inc.php,v 1.11 2007/11/08 21:00:47 kills Exp $
  */
 
-$mypage = "tinymce";
+$mypage = 'tinymce';
 
-$REX['ADDON']['rxid'][$mypage] = "52";
+$REX['ADDON']['rxid'][$mypage] = '52';
 $REX['ADDON']['page'][$mypage] = $mypage;
-$REX['ADDON']['name'][$mypage] = "TinyMCE";
-$REX['ADDON']['perm'][$mypage] = "tiny_mce[]";
-$REX['ADDON']['version'][$mypage] = "1.0";
-$REX['ADDON']['author'][$mypage] = "Wolfgang Hutteger, Markus Staab";
+$REX['ADDON']['name'][$mypage] = 'TinyMCE';
+$REX['ADDON']['perm'][$mypage] = 'tiny_mce[]';
+$REX['ADDON']['version'][$mypage] = '1.0';
+$REX['ADDON']['author'][$mypage] = 'Wolfgang Hutteger, Markus Staab, Dave Holloway';
 
 $I18N_A52 = new i18n($REX['LANG'], $REX['INCLUDE_PATH'].'/addons/'.$mypage.'/lang/');
 
