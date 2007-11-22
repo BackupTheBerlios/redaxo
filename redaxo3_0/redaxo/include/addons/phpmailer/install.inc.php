c01f33d74f1c8c7cce6b2236c33f4b9cb2597809
kills <kills>
2007-11-22 21:12:10 +0000

    doofer fehler gefixed

--- redaxo3_0/redaxo/include/addons/phpmailer/install.inc.php
+++ redaxo3_0/redaxo/include/addons/phpmailer/install.inc.php
@@ -7,10 +7,12 @@
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  *
  * @package redaxo4
- * @version $Id: install.inc.php,v 1.5 2007/10/31 18:28:27 kills Exp $
+ * @version $Id: install.inc.php,v 1.6 2007/11/22 21:12:10 kills Exp $
  */
 
 $error = '';
+
+$settings_file = $REX['INCLUDE_PATH'] .'/addons/phpmailer/classes/class.rex_mailer.inc.php';
 
 if(($state = rex_is_writable($settings_file)) !== true)
   $error = $state;
