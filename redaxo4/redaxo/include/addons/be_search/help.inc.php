fafd2ed8c993d7174bb0d6cacc2c8491b6fd8180
kills <kills>
2007-12-29 17:57:05 +0000

    erste testversion des addons; modulsuche fehlt noch

---
+++ redaxo4/redaxo/include/addons/be_search/help.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Backend Search Addon
+ *
+ * @author staab[at]public-4u[dot]de Markus Staab
+ * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
+ *
+ * @package redaxo4
+ * @version $Id: help.inc.php,v 1.1 2007/12/29 17:57:05 kills Exp $
+ */
+?>
+<b>Backend Search Addon</b>
+
+<br /><br />
+Addon zur erweiterten und schnelleren Suche von Artikeln, Kategorien und Modulen im Backend
