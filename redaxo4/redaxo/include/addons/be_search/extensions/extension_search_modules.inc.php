a143eb673e56da406a13982a1796c5e62e062b2a
kills <kills>
2007-12-29 18:00:53 +0000

    rumpf eingefügt

--- redaxo4/redaxo/include/addons/be_search/extensions/extension_search_modules.inc.php
+++ redaxo4/redaxo/include/addons/be_search/extensions/extension_search_modules.inc.php
@@ -6,6 +6,11 @@
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  *
  * @package redaxo4
- * @version $Id: extension_search_modules.inc.php,v 1.1 2007/12/29 17:57:05 kills Exp $
+ * @version $Id: extension_search_modules.inc.php,v 1.2 2007/12/29 18:00:53 kills Exp $
  */
+
+function rex_a256_search_modules()
+{
+  // TODO Implementierung fehlt noch komplett
+}
 ?>
