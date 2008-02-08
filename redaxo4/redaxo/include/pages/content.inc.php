da182f452a810aafa379fe49d1559f2789115451
kills <kills>
2008-02-08 18:31:04 +0000

    statt target="_blank" jetzt onclick="window.open(this.href); return false;"

--- redaxo4/redaxo/include/pages/content.inc.php
+++ redaxo4/redaxo/include/pages/content.inc.php
@@ -4,7 +4,7 @@
 /**
  * Verwaltung der Inhalte. EditierModul / Metadaten ...
  * @package redaxo4
- * @version $Id: content.inc.php,v 1.2 2007/12/29 17:55:31 kills Exp $
+ * @version $Id: content.inc.php,v 1.3 2008/02/08 18:33:34 kills Exp $
  */
 
 /*
@@ -627,7 +627,7 @@ if ($article->getRows() == 1)
             <ul class="rex-cnt-nav">
               <li>' . $menu_edit . ' | </li>
               <li>' . $menu_meta . ' | </li>
-              <li><a href="../index.php?article_id=' . $article_id . '&amp;clang=' . $clang . '" target="_blank"'. rex_tabindex() .'>' . $I18N->msg('show') . '</a></li>
+              <li><a href="../index.php?article_id=' . $article_id . '&amp;clang=' . $clang . '" onclick="window.open(this.href); return false;" '. rex_tabindex() .'>' . $I18N->msg('show') . '</a></li>
             </ul>';
     // ------------------------------------------ END: CONTENT HEAD MENUE
 
