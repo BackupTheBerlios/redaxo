a9eee652f0ec6c6dca65be9ded5c7b2827cd9b43
kills <kills>
2008-02-17 12:44:15 +0000

    update des phpmailers auf 2.0.0

--- redaxo4/redaxo/include/addons/phpmailer/config.inc.php
+++ redaxo4/redaxo/include/addons/phpmailer/config.inc.php
@@ -2,17 +2,17 @@
 
 /**
  * PHPMailer Addon
- *  
+ *
  * @author staab[at]public-4u[dot]de Markus Staab
- * 
+ *
  * @package redaxo4
- * @version $Id: config.inc.php,v 1.1 2007/12/28 10:45:09 kills Exp $
+ * @version $Id: config.inc.php,v 1.2 2008/02/17 12:44:39 kills Exp $
  */
 
 $mypage = 'phpmailer';
 
 $REX['ADDON']['rxid'][$mypage] = '93';
-$REX['ADDON']['page'][$mypage] = $mypage;    
+$REX['ADDON']['page'][$mypage] = $mypage;
 $REX['ADDON']['name'][$mypage] = 'PHPMailer';
 $REX['ADDON']['perm'][$mypage] = 'phpmailer[]';
 $REX['ADDON']['version'][$mypage] = "1.0";
@@ -20,9 +20,9 @@ $REX['ADDON']['author'][$mypage] = "Brent R. Matzelle, Markus Staab";
 // $REX['ADDON']['supportpage'][$mypage] = "";
 
 $REX['PERM'][] = 'phpmailer[]';
-$I18N_A93 = new i18n($REX['LANG'], $REX['INCLUDE_PATH'].'/addons/'.$mypage.'/lang/'); 
+$I18N_A93 = new i18n($REX['LANG'], $REX['INCLUDE_PATH'].'/addons/'.$mypage.'/lang/');
 
-require_once($REX['INCLUDE_PATH']. '/addons/phpmailer/classes/class.phpmailer.inc.php');
+require_once($REX['INCLUDE_PATH']. '/addons/phpmailer/classes/class.phpmailer.php');
 require_once($REX['INCLUDE_PATH']. '/addons/phpmailer/classes/class.rex_mailer.inc.php');
 
 ?>
