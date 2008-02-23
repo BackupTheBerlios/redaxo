044a1a7f507c46dbb8b10d71a771e9cf09a56078
kills <kills>
2008-02-23 14:56:00 +0000

    treffer in allen kategorien unterhalb der aktuellen

--- redaxo4/redaxo/include/addons/be_search/extensions/extension_search_mpool.inc.php
+++ redaxo4/redaxo/include/addons/be_search/extensions/extension_search_mpool.inc.php
@@ -6,7 +6,7 @@
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  *
  * @package redaxo4
- * @version $Id: extension_search_mpool.inc.php,v 1.3 2008/02/23 14:28:25 kills Exp $
+ * @version $Id: extension_search_mpool.inc.php,v 1.4 2008/02/23 14:56:00 kills Exp $
  */
 
 function rex_a256_search_mpool($params)
@@ -31,12 +31,17 @@ function rex_a256_search_mpool($params)
 
 function rex_a256_search_mpool_query($params)
 {
+  global $REX;
+
   $media_name = rex_request('a256_media_name', 'string');
   if($media_name == '') return $params['subject'];
 
   $subject = $params['subject'];
-  $subject = preg_replace('/WHERE/', 'WHERE (filename LIKE "%'. $media_name .'%" OR title LIKE "%'. $media_name .'%") AND', $subject);
-  return $subject;
+
+  return "SELECT *
+          FROM ".$REX['TABLE_PREFIX']."file f, ".$REX['TABLE_PREFIX']."file_category c
+          WHERE f.category_id = c.id AND c.path LIKE '%|". $params['category_id'] ."|%' AND (filename LIKE '%". $media_name ."%' OR title LIKE '%". $media_name ."%')
+          ORDER BY f.updatedate desc";
 }
 
 ?>
