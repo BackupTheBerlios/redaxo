9930c8d448792c68370d8187aebf4996eb95adc7
tbaddade <tbaddade>
2008-02-23 15:08:28 +0000

    Markup Anpassung

--- redaxo4/redaxo/include/addons/be_search/extensions/extension_search_mpool.inc.php
+++ redaxo4/redaxo/include/addons/be_search/extensions/extension_search_mpool.inc.php
@@ -6,7 +6,7 @@
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  *
  * @package redaxo4
- * @version $Id: extension_search_mpool.inc.php,v 1.4 2008/02/23 14:56:00 kills Exp $
+ * @version $Id: extension_search_mpool.inc.php,v 1.5 2008/02/23 15:08:28 tbaddade Exp $
  */
 
 function rex_a256_search_mpool($params)
@@ -23,25 +23,26 @@ function rex_a256_search_mpool($params)
     <label for="a256_media_name">'. $I18N_BE_SEARCH->msg('search_mpool_media') .'</label>
     <input type="text" name="a256_media_name" id="a256_media_name" value="'. $media_name .'" />
   </p>';
+  
+  $search_form = '
+  
+    <label for="a256_media_name">'. $I18N_BE_SEARCH->msg('search_mpool_media') .'</label>
+    <input type="text" name="a256_media_name" id="a256_media_name" value="'. $media_name .'" />
+  ';
 
-  $subject = str_replace('</p>', '</p>'. $search_form, $subject);
+  $subject = str_replace('</select>', '</select>'. $search_form, $subject);
 
   return $subject;
 }
 
 function rex_a256_search_mpool_query($params)
 {
-  global $REX;
-
   $media_name = rex_request('a256_media_name', 'string');
   if($media_name == '') return $params['subject'];
 
   $subject = $params['subject'];
-
-  return "SELECT *
-          FROM ".$REX['TABLE_PREFIX']."file f, ".$REX['TABLE_PREFIX']."file_category c
-          WHERE f.category_id = c.id AND c.path LIKE '%|". $params['category_id'] ."|%' AND (filename LIKE '%". $media_name ."%' OR title LIKE '%". $media_name ."%')
-          ORDER BY f.updatedate desc";
+  $subject = preg_replace('/WHERE/', 'WHERE (filename LIKE "%'. $media_name .'%" OR title LIKE "%'. $media_name .'%") AND', $subject);
+  return $subject;
 }
 
 ?>
