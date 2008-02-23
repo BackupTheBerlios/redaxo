8f2b5bbfad5844f58132071ed86f65b99472fd8a
kills <kills>
2008-02-23 12:39:50 +0000

    searchbar auch wenn $page nicht gesetzt

--- redaxo4/redaxo/include/addons/be_search/config.inc.php
+++ redaxo4/redaxo/include/addons/be_search/config.inc.php
@@ -7,7 +7,7 @@
  * @author <a href="http://www.public-4u.de">www.public-4u.de</a>
  *
  * @package redaxo4
- * @version $Id: config.inc.php,v 1.2 2008/02/13 10:07:03 kills Exp $
+ * @version $Id: config.inc.php,v 1.3 2008/02/23 12:39:50 kills Exp $
  */
 
 $mypage = 'be_search';
@@ -21,30 +21,26 @@ $REX['ADDON']['version'][$mypage] = '1.0';
 $REX['ADDON']['author'][$mypage] = 'Markus Staab';
 $REX['ADDON']['supportpage'][$mypage] = 'forum.redaxo.de';
 //$REX['PERM'][] = 'be_search[]';
-
 if ($REX['REDAXO'])
 {
   require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_common.inc.php';
   $I18N_BE_SEARCH = new i18n($REX['LANG'], $REX['INCLUDE_PATH'] . '/addons/' . $mypage . '/lang');
 
   // Include Extensions
-  if (isset ($page))
+  if(!isset($page) || $page == 'structure' || $page == '')
   {
-    if($page == 'structure')
-    {
-      require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_bar.inc.php';
-      rex_register_extension('PAGE_STRUCTURE_HEADER', 'rex_a256_search_bar');
-    }
-    elseif($page == 'content')
-    {
-      require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_bar.inc.php';
-      rex_register_extension('PAGE_CONTENT_HEADER', 'rex_a256_search_bar');
-    }
-    elseif ($page == 'module')
-    {
-      require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_modules.inc.php';
-      rex_register_extension('OUTPUT_FILTER', 'rex_a256_search_modules');
-    }
+    require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_bar.inc.php';
+    rex_register_extension('PAGE_STRUCTURE_HEADER', 'rex_a256_search_bar');
+  }
+  elseif($page == 'content')
+  {
+    require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_bar.inc.php';
+    rex_register_extension('PAGE_CONTENT_HEADER', 'rex_a256_search_bar');
+  }
+  elseif ($page == 'module')
+  {
+    require $REX['INCLUDE_PATH'].'/addons/be_search/extensions/extension_search_modules.inc.php';
+    rex_register_extension('OUTPUT_FILTER', 'rex_a256_search_modules');
   }
 }
 ?>
