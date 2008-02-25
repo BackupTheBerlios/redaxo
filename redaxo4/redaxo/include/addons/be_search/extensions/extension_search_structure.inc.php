2e54c8aceae9ee8781695a6324aa49d24d551f7c
kills <kills>
2008-02-25 10:21:24 +0000

    umleitung bei nur einem treffer gefixed und rechte pruefung eingebaut

--- redaxo4/redaxo/include/addons/be_search/extensions/extension_search_structure.inc.php
+++ redaxo4/redaxo/include/addons/be_search/extensions/extension_search_structure.inc.php
@@ -6,7 +6,7 @@
  * @author markus[dot]staab[at]redaxo[dot]de Markus Staab
  *
  * @package redaxo4
- * @version $Id: extension_search_structure.inc.php,v 1.5 2008/02/24 16:17:31 kills Exp $
+ * @version $Id: extension_search_structure.inc.php,v 1.6 2008/02/25 10:21:24 kills Exp $
  */
 
 function rex_a256_search_structure($params)
@@ -62,8 +62,12 @@ function rex_a256_search_structure($params)
     // Suche ergab nur einen Treffer => Direkt auf den Treffer weiterleiten
     if($foundRows == 1)
     {
-      header('Location:'. sprintf($editUrl, $search->getValue('re_id'), $a256_clang));
-      exit();
+      $OOArt = OOArticle::getArticleById($search->getValue('id'), $a256_clang);
+      if($REX_USER->hasCategoryPerm($OOArt->getCategoryId()))
+      {
+        header('Location:'. sprintf($editUrl, $OOArt->getCategoryId(), $a256_clang));
+        exit();
+      }
     }
     // Mehrere Suchtreffer, Liste anzeigen
     else if($foundRows > 0)
