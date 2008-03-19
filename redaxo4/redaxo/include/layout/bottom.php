27629f04192a24e3bcbe6f32df8a0f02b5144dd4
kills <kills>
2008-03-19 13:03:13 +0000

    tabindex fuer sinnlosese aenderung ;-)

--- redaxo4/redaxo/include/layout/bottom.php
+++ redaxo4/redaxo/include/layout/bottom.php
@@ -3,7 +3,7 @@
 /**
  * Layout Fuß des Backends
  * @package redaxo4
- * @version $Id: bottom.php,v 1.4 2008/03/19 08:33:37 kristinus Exp $
+ * @version $Id: bottom.php,v 1.5 2008/03/19 13:03:13 kills Exp $
  */
 
 ?>
@@ -15,7 +15,7 @@
 
 	<div id="rex-ftr">
 		<ul>
-			<li><a href="#rex-hdr">&#94;</a> | </li>
+			<li><a href="#rex-hdr"<?php echo rex_tabindex() ?>>&#94;</a> | </li>
 			<li><a href="http://www.yakamara.de" onclick="window.open(this.href); return false;" class="black"<?php echo rex_tabindex() ?>>yakamara.de</a> | </li>
       <li><a href="http://www.redaxo.de" onclick="window.open(this.href); return false;" class="black"<?php echo rex_tabindex() ?>>redaxo.de</a> | </li>
 			<?php if(isset($REX_USER)) echo '<li><a href="index.php?page=credits" class="black">'.$I18N->msg('credits').'</a> | </li>'; ?>
